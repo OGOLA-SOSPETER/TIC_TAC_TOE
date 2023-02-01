1bdaa40ce37d3ab6a34d1d599977a77fad98b4df
OGOLA-SOSPETER <captain483sos@gmail.com>
2023-02-01 15:50:13 +0300

    Update methods

--- PythonLibrary/TicTacToe.py
+++ PythonLibrary/TicTacToe.py
@@ -8,7 +8,10 @@ game_board = {'7': '', '8': '', '9': '',
 #Player
 player = 'X'
 count = 0
-
+player1_score = 0
+player2_score = 0
+player1 = ''
+player2 = ''
 #Message on winning
 message = "Congratulations, Player " + player + ". You Won✨🎉🎉"
 
@@ -19,6 +22,24 @@ def printBoard(board):
     print(' ' + board['4'] + ' | ', board['5'] + ' | ', board['6'])
     print('--+---+--')
     print(' ' + board['1'] + ' | ', board['2'] + ' | ', board['3'])
+
+def getPlayers():
+    global player1,player2
+    print('Player1, [X], Enter your name: ')
+    player1 = input()
+    print('Player2, [O], Enter your name: ')
+    player2 = input()
+
+def Report():
+    global player1_score,player2_score
+    # The total Score
+    print('\t\t\t' + '-' * 57)
+    print('\t' * 7 + 'Tic_Tac_Toe Game Challenge.')
+    print('\t\t\t' + '-' * 57)
+
+    print(player1 + '\t' * 3, player1_score)
+    print(player2 + '\t' * 3, player2_score)
+
 
 def printboard(board):
     print("\n")
@@ -40,6 +61,8 @@ def gameplay():
 
     global player
     global count
+    global player1_score
+    global player2_score
     for a in range(10):
         printboard(game_board)
         print("It is your turn, Player " + player + "  to play.\t Move to which position?")
@@ -63,41 +86,73 @@ def gameplay():
                 printboard(game_board)
                 print("\nGame Over.\n")
                 print(message)
+                if player == 'X':
+                    player1_score += 1
+                else:
+                    player2_score += 1
                 break
             elif game_board['4'] == game_board['5'] == game_board['6'] == player:  # across the middle
                 printboard(game_board)
                 print("\nGame Over.\n")
                 print(message)
+                if player == 'X':
+                    player1_score += 1
+                else:
+                    player2_score += 1
                 break
             elif game_board['1'] == game_board['2'] == game_board['3'] == player:  # across the bottom
                 printboard(game_board)
                 print("\nGame Over.\n")
                 print(message)
+                if player == 'X':
+                    player1_score += 1
+                else:
+                    player2_score += 1
                 break
             elif game_board['1'] == game_board['4'] == game_board['7'] == player:  # down the left side
                 printboard(game_board)
                 print("\nGame Over.\n")
                 print(message)
+                if player == 'X':
+                    player1_score += 1
+                else:
+                    player2_score += 1
                 break
             elif game_board['2'] == game_board['5'] == game_board['8'] == player:  # down the middle
                 printboard(game_board)
                 print("\nGame Over.\n")
                 print(message)
+                if player == 'X':
+                    player1_score += 1
+                else:
+                    player2_score += 1
                 break
             elif game_board['3'] == game_board['6'] == game_board['9'] == player:  # down the right side
                 printboard(game_board)
                 print("\nGame Over.\n")
                 print(message)
+                if player == 'X':
+                    player1_score += 1
+                else:
+                    player2_score += 1
                 break
             elif game_board['7'] == game_board['5'] == game_board['3'] == player:  # diagonal
                 printboard(game_board)
                 print("\nGame Over.\n")
                 print(message)
+                if player == 'X':
+                    player1_score += 1
+                else:
+                    player2_score += 1
                 break
             elif game_board['1'] == game_board['5'] == game_board['9'] == player:  # diagonal
                 printboard(game_board)
                 print("\nGame Over.\n")
                 print(message)
+                if player == 'X':
+                    player1_score += 1
+                else:
+                    player2_score += 1
                 break
 
             else:
@@ -124,5 +179,7 @@ def GameRestart():
         
         
 #Game Call
+getPlayers()
 gameplay()
 GameRestart()
+Report()
